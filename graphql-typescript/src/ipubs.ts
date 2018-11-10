8e263da0418c4f07169c487b60626393052a5843
Brian Mitchell <dr.brian.mitchell@gmail.com>
2018-11-10 12:02:44 -0500

    added graphql example

---
+++ graphql-typescript/src/ipubs.ts
@@ -0,0 +1,8 @@
+export interface IPubs{
+    id : number
+    title: string
+    cite: string
+    link: string
+    slides: string
+    abstract: string
+}
---
+++ graphql-typescript/src/server.ts
@@ -0,0 +1,61 @@
+import * as Koa from 'koa';
+import {ApolloServer, gql} from 'apollo-server-koa';
+import * as pubs from './papers.json';
+import * as loki from 'lokijs';
+import {IPubs} from './ipubs';
+import { publicDecrypt } from 'crypto';
+
+var db = new loki('my-app')
+let pubsC = db.addCollection('pubs')
+pubs.map(p => pubsC.insert(p))
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type PubResponse {
+    id : Int
+    title: String
+    cite: String
+    link: String
+    slides: String
+    abstract: String
+  }
+  type Query {
+    hello: String
+    pub( pid: Int): PubResponse
+    pubs: [PubResponse]
+    pquery (qry: String): [PubResponse]
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    pub: async (_,{pid}) => {
+      //let paper: IPubs[] = pubsC.find({id:pid})
+      let paper: IPubs[] = pubsC.findOne({id:pid})
+      return paper
+    },
+    pubs: async () => {
+      //let paper: IPubs[] = pubsC.find({id:pid})
+      let paper: IPubs[] = pubsC.find()
+      return paper
+    },
+    pquery: async (_,{qry}) => {
+      let q: IPubs[] = pubsC.find({'title': {'$regex':qry}});
+      return q
+    }
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+server.applyMiddleware({ app });
+
+const port = 3000;
+const host = 'localhost';
+
+app.listen(port, host, () =>
+  console.log(`🚀 Server ready at http://${host}:${port}${server.graphqlPath}`),
+);
